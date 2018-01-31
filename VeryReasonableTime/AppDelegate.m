6f70496691f25c27536bc7a85dacfa92ff5b154b
Aaron Malone <aaron@munge.net>
2018-01-30 22:34:55 -0600

    An apparently working program

--- VeryReasonableTime/AppDelegate.m
+++ VeryReasonableTime/AppDelegate.m
@@ -17,12 +17,29 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    
+    _statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    
+    [self updateTitle];
+    [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(updateTitle)
+                                   userInfo:nil repeats:YES];
+    [self setupMenu];
 }
-
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
 
+- (void) updateTitle {
+    double secs = [[NSDate date] timeIntervalSince1970] + 3600;
+    int beats = floor(fmod((secs / 86.4), 1000));
+    [_statusItem setTitle:[NSString stringWithFormat:@"@%i", beats]];
+}
+
+- (void)setupMenu {
+    NSMenu *menu = [[NSMenu alloc] init];
+    [menu addItemWithTitle:@"Quit" action:@selector(terminate:) keyEquivalent:@"q"];
+    _statusItem.menu = menu;
+}
 
 @end
